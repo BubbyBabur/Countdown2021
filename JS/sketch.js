635fa4f6327908bde30f791e1db7270785a679fc
Roger Fan <rogerjoeyfan@gmail.com>
2020-12-27 14:43:07 -0800

    Cool countdown

--- JS/sketch.js
+++ JS/sketch.js
@@ -4,22 +4,75 @@ const textsize = 20;
 
 let data2020;
 let streams;
-
-let testdata2020;
+let font;
 
 function preload() {
-    console.log("hi")
     data2020 = loadJSON("https://raw.githubusercontent.com/BubbyBabur/Countdown2021/master/data/2020%20Sucks%20-%20Global.json");
-
-    // I hate github pages :/
+    font = loadFont("https://raw.githubusercontent.com/BubbyBabur/Countdown2021/master/data/Lato-Bold.otf");
 }
+
+
+
+let textstr = "1"
+
+let points;
+let bounds;
+
+let nodes = [];
 
 function setup() {
 
-    console.log(testdata2020)
-
     createCanvas(windowWidth, windowHeight);
     streams = new AllStreams();
+
+
+    textFont(font);
+    textSize(300);
+    fill(255, 0, 0);
+
+    points = font.textToPoints(textstr, width / 2, height / 2);
+    bounds = font.textBounds(textstr, width / 2, height / 2);
+
+    for (const p of points) {
+        nodes.push(new Node(p.x - bounds.w / 2, p.y + bounds.h / 2));
+    }
+
+    setInterval(() => {
+
+        textFont(font);
+        textSize(300);
+        fill(255, 0, 0);
+
+
+        textstr = "" + Math.floor( 10*Math.random() );
+        points = font.textToPoints(textstr, width / 2, height / 2);
+        bounds = font.textBounds(textstr, width / 2, height / 2);
+
+        if(points.length < nodes.length) {
+            // We need less nodes!
+            let todelete = nodes.length - points.length;
+            for(let i = 0; i < todelete; i++) {
+                // delete random nodes
+                let randindex = Math.floor( Math.random() * nodes.length );
+                nodes.splice(randindex,1);
+            } 
+        } else if(points.length > nodes.length) {
+            // We need more nodes!
+            let tocopy = points.length - nodes.length;
+            for (let i = 0; i < tocopy; i++) {
+                // copy random nodes
+                let randindex = Math.floor(Math.random() * nodes.length);
+                nodes.splice(randindex, 0, new Node(nodes[randindex].target.x, nodes[randindex].target.y, nodes[randindex].pos.x, nodes[randindex].pos.y ));
+            }
+        }
+
+        // console.log(nodes.length, points.length)
+        for (let i = 0; i < points.length; i++) {
+            const p = points[i];
+            nodes[i].setTarget(p.x - bounds.w / 2, p.y + bounds.h / 2);
+        }
+        // console.log(textstr)
+    }, 1000);
 }
 
 function windowResized() {
@@ -28,7 +81,22 @@ function windowResized() {
 
 function draw() {
     background(0,0,0);
+
     streams.update();
+
+    noStroke();
+
+    // textFont(font);
+    // textSize(300);
+    fill(255, 0, 0);
+
+    text(Math.round(frameRate()), 15,15);
+
+    for(const n of nodes) {
+        n.update();
+    }
+    // fill(0,255,0);
+    // ellipse(width/2, height/2, 100,100);
 }
 
 class AllStreams {
@@ -47,7 +115,7 @@ class AllStreams {
     }
 
     randStream(x) {
-        return new Stream(x, this.randChoice(data2020).Name, Math.floor(1 + 4 * Math.random()), Math.floor(30 + 40 * Math.random()), 200 * Math.random())
+        return new Stream(x, this.randChoice(data2020).Name, Math.floor(1 + 3 * Math.random()), Math.floor(30 + 40 * Math.random()), 200 * Math.random())
     }
 
     update(){ 
@@ -167,5 +235,52 @@ class Stream {
         }
 
         this.render();
+    }
+}
+
+class Node {
+    constructor(x,y,px,py) {
+        // 
+        if(!px) {
+            this.pos = createVector(Math.random() * width, Math.random() * height);
+        } else {
+            this.pos = createVector(px,py);
+        }
+        this.target = createVector(x,y);
+
+        this.vel = createVector(0,0);
+        this.maxvel = 10;
+        this.maxforce = 3;
+        this.acc = createVector(0,0);
+    }
+
+    update() {
+
+        this.targetvel = p5.Vector.sub(this.target,this.pos);
+        let speed = this.maxvel;
+        if(this.targetvel.mag() < 100) {
+            speed = map(this.targetvel.mag(), 0, 100, 0, this.maxvel);
+        }
+        this.targetvel.normalize();
+        this.targetvel.mult(speed);
+
+        this.targetforce = p5.Vector.sub(this.targetvel, this.vel);
+        this.targetforce.limit(this.maxforce);
+
+        this.acc = this.targetforce;
+
+        this.pos.add(this.vel);
+        this.vel.add(this.acc);
+
+        this.render();
+    }
+
+    render() {
+        fill(255,0,0);
+        ellipse(this.pos.x,this.pos.y,10,10);
+    }
+
+    setTarget(x,y){
+        this.target = createVector(x,y);
     }
 }
